79a417d9ce64b56178baa63c5013fbdd2cc77db3
Unknown <2161503@my.ipleiria.pt>
2018-05-29 21:48:47 +0100

    add-edit corrigido

--- resources/views/users/partials/add-edit.blade.php
+++ resources/views/users/partials/add-edit.blade.php
@@ -1,4 +1,22 @@
-    <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+
+    <input type="hidden" name="user_id" value="{{$user->id}}"/>
+    <div class="form-group">
+        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+            <label for="name" class="col-md-4 control-label">Name</label>
+            <div class="col-md-6">
+                <input id="name" type="text" class="form-control" name="name"
+                       value="{{ old( 'name', $user->name) }}" required autofocus>
+
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                            <strong>{{ $errors->first('name') }}</strong>
+                        </span>
+                @endif
+            </div>
+        </div>
+
+
+        <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
             <div class="col-md-6">
@@ -12,44 +30,12 @@
                 @endif
             </div>
         </div>
-
-    <input type="hidden" name="user_id" value="{{$user->id}}"/>
-    <div class="form-group">
-        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">Name</label>
-
-            <div class="col-md-6">
-                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required >
-
-                @if ($errors->has('name'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('name') }}</strong>
-                    </span>
-                @endif
-            </div>
-        </div>
-
-
-        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
-            </div>
-        </div>
-
-
-        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+        <div class="form-group {{ $errors->has('phone') ? ' has-error' : '' }}">
             <label for="phone" class="col-md-4 control-label">Phone Number</label>
 
             <div class="col-md-6">
-                <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}" >
+                <input id="phone" type="text" class="form-control" name="phone"
+                       value="{{ old( 'phone', $user->phone) }}" >
 
                 @if ($errors->has('phone'))
                     <span class="help-block">
@@ -58,22 +44,8 @@
                 @endif
             </div>
         </div>
-
-        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <label for="password" class="col-md-4 control-label">Password</label>
-
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control" name="password" required>
-
-                @if ($errors->has('password'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="profile_photo">Profile Photo</label>
+        <div class="form-group ">
+            <label for="profile_photo" class="col-md-4 control-label">Profile Photo</label>
             <div class="col-md-6">
                 <input type="file" name="profile_photo" accept="image/*">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
